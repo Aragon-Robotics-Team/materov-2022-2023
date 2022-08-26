bb7b7e4dd3806813b84a98a3240bafbf75be3c53
Sannie Wan <sanniewan@gmail.com>
2022-08-25 17:56:31 -0700

    serial test between python and arduino to arduino

---
+++ serialTests/enterANumber.py
@@ -0,0 +1,13 @@
+from serial import Serial
+
+def main():
+    arduino = Serial(port='/dev/cu.usbmodem143101', baudrate=115200, timeout=1)
+    num = input("enter a number: ")
+    arduino.write(num)
+    while (arduino.in_waiting == 0):
+        pass
+    recieved = arduino.readline().decode("ascii")  # read arduino data with timeout = 1
+    print(recieved)
+
+if __name__ == '__main__':
+    main()
