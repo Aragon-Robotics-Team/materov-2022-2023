91df0f320ca99a4e76912f9986ab87ab85301135
Sannie Wan <sanniewan@gmail.com>
2022-09-19 17:03:50 -0700

    nav structure

--- serialTests/enterANumber.py
+++ serialTests/enterANumber.py
@@ -1,11 +1,14 @@
 from serial import Serial
 
 def main():
-    arduino = Serial(port='/dev/cu.usbmodem143101', baudrate=115200, timeout=1)
-    num = input("enter a number: ")
+    arduino = Serial(port='/dev/cu.usbmodem14301', baudrate=9600, timeout=1)
+    num = input("enter a number: ").encode("ascii")
     arduino.write(num)
+    # data sent to arduino
+    # waits for arduino to do something
     while (arduino.in_waiting == 0):
         pass
+
     recieved = arduino.readline().decode("ascii")  # read arduino data with timeout = 1
     print(recieved)
 
