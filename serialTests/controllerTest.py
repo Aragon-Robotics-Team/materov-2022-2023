b0429c70ea7f7960c1b134cbdcbc488e372adbf4
Sannie Wan <sanniewan@gmail.com>
2022-09-29 19:53:50 -0700

    thingy

--- serialTests/controllerTest.py
+++ serialTests/controllerTest.py
@@ -1,10 +1,29 @@
 from calendar import c
 from multiprocessing.forkserver import connect_to_new_process
 import pygame 
+from time import sleep
 pygame.init()
 pygame.joystick.init()
 pygame.display.init()
+
+
+while True: 
+    print("not detected")
+    pygame.event.get()
+    if pygame.joystick.get_count() > 0:
+        break
+
 controller = pygame.joystick.Joystick(0)
 controller.init()
+print("Controller name:" + controller.get_name())
 
-print(controller.name)
+while True:
+    pygame.event.pump()
+    print(str(controller.get_axis(0)))
+    print(str(controller.get_axis(1)))
+    print(str(controller.get_axis(2)))
+    print(str(controller.get_axis(3)))
+    print(str(controller.get_axis(4)))
+    print(str(controller.get_axis(5)))
+
+    sleep(0.3)
