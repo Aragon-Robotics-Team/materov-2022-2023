0c69c617745d61db248b3b15337d2c2c8f39d7f0
vfan888 <59328308+vfan888@users.noreply.github.com>
2023-01-24 18:05:54 -0800

    basic time tracker for the float -hannah

---
+++ webServer/basicFrame.js
@@ -0,0 +1,8 @@
+var http = require('http'); //create the server with module
+ http.createServer(function (req, res) { 
+   res.writeHead(200, {'Content-Type': 'text/html'}); //(200 = ok status) sets the http status code of the response, which indicates how well an http request was handled by the server
+   res.end('Hello World!'); //writes the http response back --> returns data the server has to return
+ }).listen(8080); //binds the server to a network address
+
+ //type http://localhost:8080/ into webserver to run webserver but not the html file 
+
---
+++ webServer/recievingData.py
@@ -0,0 +1,7 @@
+# returns the current time
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
