d535a4c81f778a5feb270f5054f1af8fa09f2584
iceninja3 <98727663+iceninja3@users.noreply.github.com>
2023-04-04 09:27:40 -0700

    Actuator code
    copy pasted from somewhere else because I forgot how to move files in github :)

---
+++ float/actuator.ino
@@ -0,0 +1,47 @@
+int ControlPin1 = 9; // 
+int ControlPin2= 10; // 
+int Sec = 0; //Should be =1000 for actual dleays to happen
+int setSec = 9*Sec; //90 seconds to put Float at its place
+int sinkSec = 3*Sec; //30 seconds to sink
+int floatSec = sinkSec; //for now sinking time and floating time
+
+void setup(){
+Serial.begin(9600);
+pinMode(ControlPin1, OUTPUT);
+pinMode(ControlPin2, OUTPUT);
+}
+
+void loop(){
+    delay(setSec); //time for ROV to put float in its place
+    sinkFloat(); 
+    delay(sinkSec); //time for ROV to sink to bottom of pool
+    floatyFloat();
+    delay(floatSec); //time for ROV to float to top of pool
+    sinkFloat();
+    delay(sinkSec); //time for ROV to sink to bottom 2nd time
+    floatyFloat();
+  } //will keep repeating this until it is picked back up by ROV or MATE scuba diver person
+
+void sinkFloat() { // pull in water/retract Actuator
+digitalWrite(ControlPin2, LOW);
+digitalWrite(ControlPin1, HIGH);
+delay(3000); // max range is 30000, mid point is 15000
+}
+
+void floatyFloat() { // pull in water/retract Actuator
+digitalWrite(ControlPin2, HIGH);
+digitalWrite(ControlPin1, LOW);
+delay(3000); // max range is 30000, mid point is 15000
+}
+
+void InitialPosition(){
+digitalWrite(ControlPin2, HIGH);
+digitalWrite(ControlPin1, LOW);
+delay(30000);
+}
+
+void stopActuator(){
+digitalWrite(ControlPin2, LOW);
+digitalWrite(ControlPin1, LOW);
+delay(7000);
+}
